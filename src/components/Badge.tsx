ce345569529026622c4f971cd97892036c5b8f3b
ForgeFlow AI <nickcprince@gmail.com>
2024-05-05 13:17:39 -0500

    branding changes

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,17 +44,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Made by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.forgeflow.ai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Forgeflow AI</span>
       </a>
     </span>
   );
